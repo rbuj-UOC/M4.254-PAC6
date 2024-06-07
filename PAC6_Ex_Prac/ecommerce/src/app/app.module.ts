9cccd1700d44d52a7eb2f835d650f142eb07e2d5
rbuj <robert.buj@gmail.com>
2024-06-08 00:40:31 +0200

    Exercici 2: treu ArticleNewTemplateComponent

--- PAC6_Ex_Prac/ecommerce/src/app/app.module.ts
+++ PAC6_Ex_Prac/ecommerce/src/app/app.module.ts
@@ -1,28 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { ArticleItemComponent } from './articles/article-item/article-item.component';
 import { ArticleListComponent } from './articles/article-list/article-list.component';
-import { NavbarComponent } from './articles/navbar/navbar.component';
-import { ArticleNewTemplateComponent } from './articles/article-new-template/article-new-template.component';
 import { ArticleNewReactiveComponent } from './articles/article-new-reactive/article-new-reactive.component';
+import { NavbarComponent } from './articles/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleItemComponent,
     ArticleListComponent,
-    NavbarComponent,
-    ArticleNewTemplateComponent,
-    ArticleNewReactiveComponent
+    ArticleNewReactiveComponent,
+    NavbarComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
